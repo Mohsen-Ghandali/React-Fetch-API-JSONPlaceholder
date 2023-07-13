4d91214aad60f26b8d5f735844feb521ed3de9c9
Mohsen Ghandali <84251780+Mohsen-Ghandali@users.noreply.github.com>
2023-07-13 11:50:33 +0200

    Update App.jsx

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -5,39 +5,31 @@ const App = () => {
 
   useEffect(() => {
     console.log("trigger posts");
-
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => {
-        setPosts(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    fetch("https://jsonplaceholder.typicode.com/posts").then(data => data.json()).then(res => {
+      setPosts(res)
+    }).catch(error => console.log(error))
 
     //------start clean up----
     return () => {
       console.log("trigger clean up");
-      setPosts(null);
-    };
-
+      setPosts(null)
+    }
   }, []);
   //------end clean up----
+
 
   return (
     <div className="container-fluid text-white mt-2">
       <div className="row m-0 justify-content-center">
-        {posts === null
-          ? ""
-          : posts.map((elem) => (
-            <div
-              className="col-5 bg-dark p-3 m-1 shadow-lg rounded-2"
-              key={elem.id}
-            >
-              <h4>{elem.title.substr(0, 30)}</h4>
-              <p>{elem.body.substr(0, 100)}</p>
-            </div>
-          ))}
+        {posts === null ? "" : posts.map((elem) => (
+          <div
+            className="col-5 bg-dark p-3 m-1 shadow-lg rounded-2"
+            key={elem.id}
+          >
+            <h4>{elem.title.substr(0, 30)}</h4>
+            <p>{elem.body.substr(0, 100)}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
